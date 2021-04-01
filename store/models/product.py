4daaa72d3f1b9bc472a7d5e41aa0614f57b0d69a
Jay28497 <jaykanjariya28@gmail.com>
2021-04-01 19:00:56 +0530

    show product on fronted

--- store/models/product.py
+++ store/models/product.py
@@ -9,6 +9,10 @@ class Product(models.Model):
     description = models.CharField(max_length=200, default='')
     image = models.ImageField(upload_to='uploads/products/')
 
+    @staticmethod
+    def get_all_products():
+        return Product.objects.all()
+
     def __str__(self):
         return self.name
 
--- store/views.py
+++ store/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models.product import Product
+
 
 # Create your views here.
+def index(request):
+    products = Product.get_all_products()
+    return render(request, 'store/index.html', {'products': products})
