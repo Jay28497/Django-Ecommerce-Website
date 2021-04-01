3029d77834fd1ef8b38c07bccba353478f82d327
Jay28497 <jaykanjariya28@gmail.com>
2021-04-01 13:46:33 +0530

    added category model

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models.product import Product
+from .models.category import Category
+
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category', 'description', 'image']
+
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name']
+
 
 # Register your models here.
+admin.site.register(Product, AdminProduct)
+admin.site.register(Category, AdminCategory)
